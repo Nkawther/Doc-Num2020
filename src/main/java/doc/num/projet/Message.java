5319a5b6a86edb1e4be0c7320cd20465408ea6b5
Nkawther <kawter.namane@outlook.fr>
2020-10-22 14:09:08 +0200

    First push - ClassMessage and ClassAttributRole

---
+++ src/main/java/doc/num/projet/Message.java
@@ -0,0 +1,38 @@
+package doc.num.projet;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Data
+@Entity
+
+public class Message{
+
+    @Id
+
+    Long id;
+    Date date;
+    Date validity;
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Enumerated(EnumType.STRING)
+    public Set<AttributRole> roles = new HashSet<>();
+
+    public Message(){
+
+    }
+    public Message(Date date, Date validity){
+        this.date = date;
+        this.validity=validity;
+        this.roles.add(AttributRole.MESSAGE);
+    }
+}
