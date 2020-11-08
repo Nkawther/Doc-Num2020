ccec4f8bb044238e322be519168ebdaab8faa5e0
Nkawther <kawter.namane@outlook.fr>
2020-11-08 14:14:15 +0100

    Add page writing and reading into acontroller

---
+++ src/main/java/doc/num/projet/controller/ControllerInit.java
@@ -0,0 +1,36 @@
+package doc.num.projet.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+
+@Controller
+public class ControllerInit {
+    
+    
+
+    @RequestMapping("/")
+    public String enterInside(){
+        return "redirect:index";
+    }
+     
+    @RequestMapping("/index")
+    public String afficher(Model m){
+        m.addAttribute("pageName", "Index");
+        return "index";
+    }
+
+    @RequestMapping("/writing")
+    public String writing(Model m){
+        m.addAttribute("pageName", "Writing XML");
+        return "writing";
+    } 
+    @RequestMapping("/reading")
+    public String creading(Model m){
+        m.addAttribute("pageName", "Reading XML");
+        return "reading";
+    }
+}
