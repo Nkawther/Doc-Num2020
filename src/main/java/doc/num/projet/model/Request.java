98627858f1cc02102413501457ae63f87f0e2131
Nkawther <kawter.namane@outlook.fr>
2020-11-08 14:13:36 +0100

    Modify type of idprevmsg and add super for extends message

--- src/main/java/doc/num/projet/model/Request.java
+++ src/main/java/doc/num/projet/model/Request.java
@@ -2,6 +2,7 @@ package doc.num.projet.model;
 
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import javax.persistence.Entity;
@@ -20,17 +21,19 @@ public class Request extends Message{
     Long id;
     @OneToMany
     List<Object> rcvObjectList = new ArrayList<Object>();
-    Long idPrevMsg;
+    String idPrevMsg;
 
     public Request(){
-
+        super();
     }
-    public Request(Long idPrevMsg, List<Object> rcvObjectList){
+    public Request(Date date, Date dateV, String idPrevMsg, List<Object> rcvObjectList){
+        super(date, dateV);
         this.idPrevMsg=idPrevMsg;
         this.rcvObjectList=rcvObjectList;
         this.roles.add(AttributRole.REQUEST);
     }
-    public Request(Long idPrevMsg, Object rcvObject){
+    public Request(Date date, Date dateV, String idPrevMsg, Object rcvObject){
+        super(date, dateV);
         this.idPrevMsg=idPrevMsg;
         this.rcvObjectList.add(rcvObject);
         this.roles.add(AttributRole.REQUEST);
