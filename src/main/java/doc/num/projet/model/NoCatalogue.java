953002629bf79609550105a59bef3b6bd19b446a
Nkawther <kawter.namane@outlook.fr>
2020-11-19 09:50:01 +0100

    Nocat

--- src/main/java/doc/num/projet/model/NoCatalogue.java
+++ src/main/java/doc/num/projet/model/NoCatalogue.java
@@ -3,9 +3,6 @@ package doc.num.projet.model;
 import java.util.Date;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
 @Entity
 public class NoCatalogue extends Message {
